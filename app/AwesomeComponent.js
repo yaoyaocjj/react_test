f78fc408b6f1d31ddf7646427cbf95e4d941bb4c
yaoyoacjj <cjj022592@163.com>
2017-08-08 17:17:58 +0800

    test

--- app/AwesomeComponent.js
+++ app/AwesomeComponent.js
@@ -16,7 +16,7 @@ export default class AwesomeComponent extends React.Component {
 		return (
 				<div>
 					Likes: <span>{this.state.likesCount}</span>
-					<div><button onClick={this.onLike}>Like Me and</button></div>
+					<div><button onClick={this.onLike}>Like Me</button></div>
 				</div>
 		);
 	}
