7e7eb21e0a1d6335d052f52209a067c8c0d23faa
yaoyoacjj <cjj022592@163.com>
2017-08-10 10:48:32 +0800

    fixed untracked files

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import AwesomeComponent from './AwesomeComponent';
 import React from 'react';
 import { render } from 'react-dom';
+import FunctionAsChild from './FunctionAsChild';
 
-render(<AwesomeComponent/>, document.getElementById('app'));
+render(<AwesomeComponent/>, <FunctionAsChild/>, document.getElementById('app'));
